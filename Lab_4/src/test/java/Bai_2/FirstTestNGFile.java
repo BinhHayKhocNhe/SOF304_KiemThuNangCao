bb2a8773fbe991191634db6861e608ef39db1166
BinhHayKhocNhe <clonechuaxu1@gmail.com>
2024-03-18 16:51:53 +0700

    add lab 4

---
+++ Lab_4/src/test/java/Bai_2/FirstTestNGFile.java
@@ -0,0 +1,10 @@
+package Bai_2;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNGFile {
+  @Test
+  public void f() {
+	  System.out.println("Output Test Case 1");
+  }
+}
---
+++ Lab_4/src/test/java/Bai_3/VatCalculator.java
@@ -0,0 +1,9 @@
+package Bai_3;
+
+public class VatCalculator {
+
+	public double getVatOnAmount(double	amount) {
+		return amount*0.1;
+	}
+
+}
